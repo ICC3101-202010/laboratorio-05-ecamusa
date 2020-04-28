a41045fb2f0730e80ec7f4a50c4066ee0379f48b
ecamusa <epcamus@miuandes.cl>
2020-04-27 22:31:53 -0400

    Laboratorio5

---
+++ Laboratorio 5/Laboratorio 5/Class3.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Solucion_Lab_21_abril
+{
+    public class MailSender
+    {   
+        public void OnRegistered(object source, RegisterEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nCorreo enviado a {e.Email}: \n Gracias por registrarte, {e.Username}!\n Por favor, para poder verificar tu correo, has click en: {e.VerificationLink}\n");
+            Thread.Sleep(2000);
+
+            EmailSents();
+        }
+
+        public void OnPasswordChanged(object source, ChangePasswordEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nCorreo enviado a {e.Email}:  \n {e.Username}, te notificamos que la contrasena de tu cuenta PlusCorp ha sido cambiada. \n");
+            Thread.Sleep(2000);
+        }
+
+        public delegate void EmailSentEventHandler(object source, EventArgs args);
+        public event EmailSentEventHandler EmailSent;
+
+        protected virtual void EmailSents()
+        {
+            if (EmailSent != null)
+                EmailSent(this, EventArgs.Empty);
+        }
+
+
+
+
+    }
+}
---
+++ Laboratorio 5/Laboratorio 5/Class8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solucion_Lab_21_abril
+{
+    public class User
+    {
+
+
+        public delegate void EmailVerifiedEventHandler(object source, EventArgs args);
+        public event EmailVerifiedEventHandler EmailVerify;
+
+
+
+        protected virtual void OnEmailVerified()
+        {
+            if (EmailVerify != null)
+            {
+                EmailVerify(this, EventArgs.Empty);
+            }
+
+        }
+
+        public void OnEmailSent(object source, EventArgs args)
+        {
+            string answer;
+            Console.WriteLine("presione 1 si quiere verificar su correo");
+            answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                OnEmailVerified();
+            }
+
+        }
+    }
+
+}
